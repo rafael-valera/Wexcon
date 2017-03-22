67c65e37188a35c30bb636eb2dd543bcdd19ee43
Rafael Valera <rvalera_2009@hotmail.com>
2017-03-22 21:30:47 +0100

    adds new TickerFrame import, removes '.py' from string in variable __all__ for consistency

--- btceconnect/__init__.py
+++ btceconnect/__init__.py
@@ -1,7 +1,7 @@
 from btceconnect.account import Account
-
 from btceconnect.pair import Pair
 from btceconnect.trader import Trader
+from btceconnect.ticker_frame import TickerFrame
 from btceconnect.constants import *
 
-__all__ = ["trader", "account.py", "pair"]
+__all__ = ["trader", "account", "pair"]

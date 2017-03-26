664517e97d30f47fd309b6df21e46eb49b0e4444
Rafael Valera <rvalera_2009@hotmail.com>
2017-03-26 23:48:49 +0200

    adds tests for the Account class and Pair class

---
+++ btceconnect/tests/test_account.py
@@ -0,0 +1,53 @@
+import unittest
+
+from btceconnect.account import Account
+
+
+class TestAccount(unittest.TestCase):
+    def setUp(self):
+        self.get_info_response = {
+            "success": 1,
+            "return": {
+                "funds": {
+                    "usd": 325,
+                    "btc": 23.998,
+                    "ltc": 0,
+                },
+                "rights": {
+                    "info": 1,
+                    "trade": 0,
+                    "withdraw": 0
+                },
+                "transaction_count": 15,
+                "open_orders": 18,
+                "server_time": 1342123547
+            }}
+        self.account = Account(self.get_info_response)
+
+    def test_update_account_fundsSetup(self):
+        usd_funds = self.account.funds.usd
+        expected_usd_funds = 325
+        self.assertEqual(expected_usd_funds, usd_funds)
+
+    def test_update_account_rightsSetup(self):
+        info_right = self.account.rights.info
+        expected_info_right = 1
+        self.assertEqual(expected_info_right, info_right)
+
+    def test_update_account_openOrdersSetup(self):
+        open_orders = self.account.open_orders
+        expected_open_orders = 18
+        self.assertEqual(expected_open_orders, open_orders)
+
+    def test_update_account_transactionCount(self):
+        transactions = self.account.transaction_count
+        expected_transactions = 15
+        self.assertEqual(expected_transactions, transactions)
+
+    def test_update_account_serverTime(self):
+        time = self.account.server_time
+        expected_time = 1342123547
+        self.assertEqual(expected_time, time)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ btceconnect/tests/test_pair.py
@@ -0,0 +1,86 @@
+import unittest
+
+from btceconnect.pair import Pair
+
+
+class TestPair(unittest.TestCase):
+    def setUp(self):
+        self.info = {
+            "server_time": 1370814956,
+            "pairs": {
+                "btc_usd": {
+                    "decimal_places": 3,
+                    "min_price": 0.1,
+                    "max_price": 400,
+                    "min_amount": 0.01,
+                    "hidden": 0,
+                    "fee": 0.2
+                },
+                "btc_eur": {
+                    "decimal_places": 3,
+                    "min_price": 0.1,
+                    "max_price": 400,
+                    "min_amount": 0.01,
+                    "hidden": 0,
+                    "fee": 0.2
+                },
+                "eur_usd": {
+                    "decimal_places": 3,
+                    "min_price": 0.1,
+                    "max_price": 400,
+                    "min_amount": 0.01,
+                    "hidden": 0,
+                    "fee": 0.2
+                }
+
+            }
+        }
+
+        self.ticker = {
+            "btc_usd": {
+                "high": 109.88,
+                "low": 91.14,
+                "avg": 100.51,
+                "vol": 1632898.2249,
+                "vol_cur": 16541.51969,
+                "last": 101.773,
+                "buy": 101.9,
+                "sell": 101.773,
+                "updated": 1370816308
+            },
+            "btc_eur": {
+                "high": 109.88,
+                "low": 91.14,
+                "avg": 100.51,
+                "vol": 1632898.2249,
+                "vol_cur": 16541.51969,
+                "last": 101.773,
+                "buy": 101.9,
+                "sell": 101.773,
+                "updated": 1370816308
+            },
+            "eur_usd": {
+                "high": 109.88,
+                "low": 91.14,
+                "avg": 100.51,
+                "vol": 1632898.2249,
+                "vol_cur": 16541.51969,
+                "last": 101.773,
+                "buy": 101.9,
+                "sell": 101.773,
+                "updated": 1370816308
+            }
+        }
+
+        self.btc_usd = Pair("btc_usd", self.info, self.ticker)
+
+    def test_getAvailablePairs_checkPairs(self):
+        pairs = sorted(self.btc_usd.get_available_pairs(self.info))
+        expected_pairs = sorted(["btc_usd", "btc_eur", "eur_usd"])
+        self.assertListEqual(expected_pairs, pairs)
+
+    def test_updatePair_checkTicker(self):
+        self.btc_usd.update_pair(self.info, self.ticker)
+        buy_price = self.btc_usd.ticker.buy
+        expected_buy_price = 101.9
+        self.assertEqual(expected_buy_price, buy_price)

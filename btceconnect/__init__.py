cbf061df1d5dc7d450e361e42a25975b2f550a6c
Rafael Valera <rvalera_2009@hotmail.com>
2017-03-28 22:41:02 +0000

    removes gui class import

--- btceconnect/__init__.py
+++ btceconnect/__init__.py
@@ -1,7 +1,6 @@
 from btceconnect.account import Account
+from btceconnect.constants import *
 from btceconnect.pair import Pair
 from btceconnect.trader import Trader
-from btceconnect.ticker_frame import TickerFrame
-from btceconnect.constants import *
 
 __all__ = ["trader", "account", "pair"]
